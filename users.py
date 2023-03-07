20552533539e9585dc086c7ae8c1c4b35c213be6
Vincent Liao <vincent.liao@protonmail.com>
2023-03-07 14:20:29 +0700

    update: modularize functions in users.py

--- users.py
+++ users.py
@@ -12,7 +12,7 @@ warpcast_hub_key = os.getenv("WARPCAST_HUB_KEY")
 
 def get_recent_users(cursor: str = None):
     # have cursor in url if cursor exists, use ternary
-    url = f"https://api.warpcast.com/v2/recent-users?cursor={cursor}&limit=1000" if cursor else "https://api.warpcast.com/v2/recent-users?limit=1000"
+    url = f"https://api.warpcast.com/v2/recent-users?cursor={cursor}&limit=1000" if cursor else "https://api.warpcast.com/v2/recent-users?limit=10"
 
     print(f"Fetching from {url}")
 
@@ -26,6 +26,35 @@ def get_recent_users(cursor: str = None):
             "cursor": json_data.get("next", {}).get('cursor') if json_data.get("next") else None}
 
 
+def get_location(session, user):
+    if 'location' in user['profile']:
+        place_id = user['profile']['location'].get('placeId')
+        if place_id:
+            l = session.query(Location).filter_by(
+                place_id=place_id).first()
+            if not l:
+                l = Location(place_id=place_id,
+                             description=user['profile']['location']['description'])
+                session.merge(l)
+
+            return l
+
+    return None
+
+
+def get_warpcast_data(user):
+    return {
+        'fid': user['fid'],
+        'username': user['username'],
+        'display_name': user['displayName'],
+        'verified': user['pfp']['verified'] if 'pfp' in user else 0,
+        'pfp_url': user['pfp']['url'] if 'pfp' in user else '',
+        'follower_count': user['followerCount'],
+        'following_count': user['followingCount'],
+        'bio_text': user['profile']['bio']['text'] if 'bio' in user['profile'] else None,
+    }
+
+
 def insert_users_to_db(engine, start_cursor=None):
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -37,31 +66,9 @@ def insert_users_to_db(engine, start_cursor=None):
         cursor = data.get("cursor")
 
         for user in users:
-            u = User(
-                fid=user['fid'],
-                username=user['username'],
-                display_name=user['displayName'],
-                verified=user['pfp']['verified'] if 'pfp' in user else 0,
-                pfp_url=user['pfp']['url'] if 'pfp' in user else '',
-                follower_count=user['followerCount'],
-                following_count=user['followingCount'],
-                bio_text=user['profile']['bio']['text'] if 'bio' in user['profile'] else None,
-                registered_at=-1,  # will fetch this data from searchcaster
-                farcaster_address='',  # will fetch this data from searchcaster
-            )
-
-            if 'location' in user['profile']:
-                place_id = user['profile']['location'].get('placeId')
-                if place_id:
-                    l = session.query(Location).filter_by(
-                        place_id=place_id).first()
-                    if not l:
-                        l = Location(place_id=place_id,
-                                     description=user['profile']['location']['description'])
-                        session.merge(l)
-
-                    u.location = l
-
+            u = User(**get_warpcast_data(user))
+            location = get_location(session, user)
+            u.location = location
             session.merge(u)
         session.commit()
 
@@ -74,13 +81,39 @@ def insert_users_to_db(engine, start_cursor=None):
     session.close()
 
 
+def update_user_data(user, data):
+    for attr, value in data.items():
+        setattr(user, attr, value)
+    return user
+
+
+def get_searchcaster_data(raw_data):
+    return {
+        'farcaster_address': raw_data['body']['address'],
+        'external_address': raw_data['connectedAddress'],
+        'registered_at': raw_data['body']['registeredAt']
+    }
+
+
+def get_ensdata_data(raw_data):
+    return {
+        'ens': raw_data.get('ens'),
+        'url': raw_data.get('url'),
+        'github': raw_data.get('github'),
+        'twitter': raw_data.get('twitter'),
+        'telegram': raw_data.get('telegram'),
+        'email': raw_data.get('email'),
+        'discord': raw_data.get('discord')
+    }
+
+
 def insert_data_from_searchcaster(engine):
     url = 'https://searchcaster.xyz/api/profiles?username='
 
     session = sessionmaker(bind=engine)()
 
     all_usernames = [u.username for u in session.query(
-        User).filter_by(registered_at=-1).all()]
+        User).filter(User.registered_at == -1).all()]
 
     for username in all_usernames:
         success = False  # flag to indicate whether the update was successful
@@ -90,22 +123,12 @@ def insert_data_from_searchcaster(engine):
                 data = result.json()
                 item = data[0]
 
-                farcaster_address = item['body']['address']
-                external_address = item['connectedAddress']
-                registered_at = item['body']['registeredAt']
-
                 user = session.query(User).filter_by(username=username).first()
-                user.farcaster_address = farcaster_address
-                user.external_address = external_address
-                user.registered_at = registered_at
-
+                user = update_user_data(user, get_searchcaster_data(item))
                 session.merge(user)
                 session.commit()
 
-                print(
-                    f"Updated {username} with {farcaster_address} {external_address} {registered_at}")
-
-                time.sleep(1)
+                print(f"Updated {username} with {data}")
 
                 success = True  # set flag to indicate success
 
@@ -135,20 +158,14 @@ def insert_data_from_ensdata(engine):
                 if data:
                     user = session.query(User).filter_by(
                         external_address=address).first()
-                    user.ens = data.get('ens')
-                    user.url = data.get('url')
-                    user.github = data.get('github')
-                    user.twitter = data.get('twitter')
-                    user.telegram = data.get('telegram')
-                    user.email = data.get('email')
-                    user.discord = data.get('discord')
+                    user = update_user_data(user, get_ensdata_data(data))
 
                     session.merge(user)
                     session.commit()
 
                     print(f"Updated {address} with {data}")
 
-                time.sleep(0.5)
+                # time.sleep(0.5)
 
                 success = True  # set flag to indicate success
 

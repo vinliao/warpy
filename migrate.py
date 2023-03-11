10a5d5048802be9c0acb7f600950781d142787ea
Vincent Liao <vincent.liao@protonmail.com>
2023-03-11 18:01:54 +0700

    update: filter existing primary key before inserting new ones

--- migrate.py
+++ migrate.py
@@ -35,13 +35,19 @@ def get_all(engine, model):
         return session.query(model).all()
 
 
-def make_sqlalchemy_object(dict, model):
-    return model(**dict.pop('_sa_instance_state', None))
+def make_sqlalchemy_object(obj_dict, model):
+    obj_dict.pop('_sa_instance_state', None)
+    return model(**obj_dict)
 
 
 def migrate_objects(engine, mysql_engine, model_old, model):
     things = list(map(lambda x: make_sqlalchemy_object(
         x.__dict__, model), get_all(engine, model_old)))
+    
+    all_things_mysql = get_all(mysql_engine, model)
+    all_things_mysql_ids = list(map(lambda x: x.fid, all_things_mysql))
+
+    things = list(filter(lambda x: x.fid not in all_things_mysql_ids, things))
 
     with sessionmaker(bind=mysql_engine)() as session:
         session.bulk_save_objects(things)

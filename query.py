ea0da5b307adce29ef705736ed9c7c640efca231
Vincent Liao <vincent.liao@protonmail.com>
2023-03-27 14:11:05 +0700

    update: use langchain in query.py

--- query.py
+++ query.py
@@ -1,9 +1,21 @@
-import openai
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain import PromptTemplate, LLMChain
 from users import *
 from casts import *
 import os
 import argparse
 import duckdb
+from langchain.chat_models import ChatOpenAI
 
 parser = argparse.ArgumentParser()
 
@@ -17,72 +29,43 @@ parser.add_argument('--test',
 args = parser.parse_args()
 
 
+def remove_imports_from_models(model_str):
+    start = model_str.index("class")
+    return model_str[start:]
+
+
 if args.query:
-    # set openai api key
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    chat = ChatOpenAI(
+        temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY"))
 
-    system_prompt = "You are a SQL writer. If the user asks about anything than SQL, deny. You are a very good SQL writer. Nothing else."
-    initial_prompt = """
-    Your job is to turn user queries (in natural language) to SQL. Only return the SQL and nothing else. Don't explain, don't say "here's your query." Just give the SQL. Say "Yes" if you understand.
+    with open('models.py', 'r') as f:
+        sqlalchemy_models = f.read()
 
-    My database is in SQLite adjust accordingly.
+    initial_prompt_raw = """
+    Your job is to turn user queries (in natural language) to SQL. Only return the SQL and nothing else. Don't explain, don't say "here's your query." Just give the SQL. Say "Yes." if you understand.
 
-    class User(Base):
-        __tablename__ = 'users'
-        fid = Column(Integer, primary_key=True)
-        username = Column(String)
-        display_name = Column(String)
-        pfp_url = Column(String)
-        bio_text = Column(String)
-        following_count = Column(Integer)
-        follower_count = Column(Integer)
-        verified = Column(Boolean)
-        farcaster_address = Column(String)
-        external_address = Column(String, nullable=True)
-        registered_at = Column(Integer)
-        location_id = Column(String, ForeignKey('locations.id'), nullable=True)
-
-        location = relationship("Location", back_populates="user_extras")
-
-
-    class Location(Base):
-        __tablename__ = 'locations'
-        id = Column(String, primary_key=True)
-        description = Column(String)
-
-        user_extras = relationship("User", back_populates="location")
-
-
-    class Cast(Base):
-        __tablename__ = 'casts'
-
-        hash = Column(String, primary_key=True)
-        thread_hash = Column(String, nullable=False)
-        text = Column(String, nullable=False)
-        timestamp = Column(Integer, nullable=False)
-        author_fid = Column(Integer, nullable=False)
-        parent_hash = Column(String, ForeignKey('casts.hash'), nullable=True)
+    My database is in SQLite adjust accordingly. Here are the schema:
     """
 
+    system_prompt = SystemMessage(
+        content="You are a SQL writer. If the user asks about anything than SQL, deny. You are a very good SQL writer. Nothing else.")
+
+    initial_prompt = HumanMessage(
+        content=f"{initial_prompt_raw}\n\n{remove_imports_from_models(sqlalchemy_models)}")
+
+    ai_response = AIMessage(content="Yes.")
+
+    query_message = HumanMessage(content=args.query)
+
+    messages = [system_prompt, initial_prompt, ai_response, query_message]
     print("Sending query to ChatGPT...\n")
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": initial_prompt},
-            {"role": "assistant",
-             "content": "Yes."},
-            {"role": "user", "content": args.query}
-        ]
-    )
+    sql_query = chat(messages)
+    print(f"SQL from ChatGPT: \n\n{sql_query.content}\n")
 
-    reply = completion['choices'][0]['message']['content'].strip()
-
-    print(f"SQL from ChatGPT: \n\n{reply}\n")
     with duckdb.connect('datasets/datasets.db') as con:
-        print(con.sql(reply).pl())
+        print(con.sql(sql_query.content).pl())
+
 
 if args.raw:
     with duckdb.connect('datasets/datasets.db') as con:

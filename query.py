ee030736a47819ed5fe65388c8b5cf0bfef15a76
Vincent Liao <vincent.liao@protonmail.com>
2023-03-31 13:18:01 +0700

    fix: gpt not doing stuff in millisecond

--- query.py
+++ query.py
@@ -35,6 +35,12 @@ def remove_imports_from_models(model_str):
     return model_str[start:]
 
 
+def get_sqlalchemy_models():
+    with open('models.py', 'r') as f:
+        sqlalchemy_models = f.read()
+    return remove_imports_from_models(sqlalchemy_models)
+
+
 if args.raw:
     with sqlite3.connect('datasets/datasets.db') as con:
         cur = con.cursor()
@@ -55,14 +61,14 @@ if args.query:
     initial_prompt_raw = """
     Your job is to turn user queries (in natural language) to SQL. Only return the SQL and nothing else. Don't explain, don't say "here's your query." Just give the SQL. Say "Yes." if you understand.
 
-    Timestamp is in unix millisecond format, database is in SQLite, adjust accordingly. Here are the schema:
+    Timestamp is in unix millisecond format, anything timestamp related must be multiplied by 1000. The database is in SQLite, adjust accordingly. Here are the schema:
     """
 
     system_prompt = SystemMessage(
         content="You are a SQL writer. If the user asks about anything than SQL, deny. You are a very good SQL writer. Nothing else.")
 
     initial_prompt = HumanMessage(
-        content=f"{initial_prompt_raw}\n\n{remove_imports_from_models(sqlalchemy_models)}")
+        content=f"{initial_prompt_raw}\n\n{get_sqlalchemy_models()}")
 
     ai_response = AIMessage(content="Yes.")
 

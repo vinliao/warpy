0672653b9057c78a5fff2b05d059eb318f40d960
Vincent Liao <vincent.liao@protonmail.com>
2023-03-31 18:01:08 +0700

    update: only run commands when there are the appropriate environment variables

--- Makefile
+++ Makefile
@@ -5,3 +5,4 @@ install:
 	. venv/bin/activate && pip install -r requirements.txt
 	@echo "Environment setup complete. Run 'source venv/bin/activate' to activate the virtual environment."
 	python main.py download
+	python main.py init
--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from query import (
 load_dotenv()
 warpcast_hub_key = os.getenv('WARPCAST_HUB_KEY')
 openai_api_key = os.getenv('OPENAI_API_KEY')
+alchemy_api_key = os.getenv('ALCHEMY_API_KEY')
 
 app = typer.Typer()
 
@@ -27,24 +28,28 @@ app.add_typer(indexer_app, name="indexer")
 
 def update_env_variables():
     dotenv_path = '.env'
-    global warpcast_hub_key, openai_api_key
+    global warpcast_hub_key, openai_api_key, alchemy_api_key
     new_warpcast_hub_key = typer.prompt("Enter your Warpcast Hub key")
     new_openai_api_key = typer.prompt("Enter your OpenAI API key")
+    new_alchemy_api_key = typer.prompt("Enter your Alchemy API key")
     set_key(dotenv_path, "WARPCAST_HUB_KEY", new_warpcast_hub_key)
     set_key(dotenv_path, "OPENAI_API_KEY", new_openai_api_key)
+    set_key(dotenv_path, "ALCHEMY_API_KEY", new_alchemy_api_key)
     warpcast_hub_key = new_warpcast_hub_key
     openai_api_key = new_openai_api_key
+    alchemy_api_key = new_alchemy_api_key
 
 
 @app.command()
 def init():
     """Initialize the environment with OpenAI and Warpcast Hub keys."""
-    global warpcast_hub_key, openai_api_key
+    global warpcast_hub_key, openai_api_key, alchemy_api_key
 
-    if warpcast_hub_key and openai_api_key:
+    if warpcast_hub_key or openai_api_key or alchemy_api_key:
         typer.echo("Environment variables detected:")
         typer.echo(f"WARPCAST_HUB_KEY: {warpcast_hub_key}")
         typer.echo(f"OPENAI_API_KEY: {openai_api_key}")
+        typer.echo(f"ALCHEMY_API_KEY: {alchemy_api_key}")
         if typer.confirm("Do you want to overwrite the existing values?"):
             update_env_variables()
     else:
@@ -54,6 +59,9 @@ def init():
 @indexer_app.command("all")
 def refresh_all_data():
     """Refresh all data in the DB."""
+    if not warpcast_hub_key or not alchemy_api_key:
+        print("Error: you need to set the environment variables WARPCAST_HUB_KEY and ALCHEMY_API_KEY. Run `python main.py init` to do so.")
+        return
     asyncio.run(user_indexer_main())
     cast_indexer_main()
     asyncio.run(eth_indexer_main())
@@ -62,18 +70,30 @@ def refresh_all_data():
 @indexer_app.command("user")
 def refresh_user_data():
     """Refresh user data."""
+    if not warpcast_hub_key:
+        print("Error: you need to set the environment variable WARPCAST_HUB_KEY. Run `python main.py init` to do so.")
+        return
+
     asyncio.run(user_indexer_main())
 
 
 @indexer_app.command("cast")
 def refresh_cast_data():
     """Refresh cast data."""
+    if not warpcast_hub_key:
+        print("Error: you need to set the environment variable WARPCAST_HUB_KEY. Run `python main.py init` to do so.")
+        return
+
     cast_indexer_main()
 
 
 @indexer_app.command("eth")
 def refresh_eth_data():
     """Refresh onchain Ethereum data."""
+    if not alchemy_api_key:
+        print("Error: you need to set the environment variable ALCHEMY_API_KEY. Run `python main.py init` to do so.")
+        return
+
     asyncio.run(eth_indexer_main())
 
 
@@ -81,10 +101,6 @@ def refresh_eth_data():
 def download():
     """Download datasets."""
     downloader_main()
-    print()
-
-    if typer.confirm("(Optional) do you want to initialize the environment with OpenAI and Warpcast Hub keys?"):
-        init()
 
 
 @app.command()
@@ -93,27 +109,15 @@ def package():
     packager_main()
 
 
-# # TODO: --csv flag
-# @app.command()
-# def query(query: str = typer.Argument(None, help='Query Farcaster data with natural language.'),
-#           raw: str = typer.Option(None, help='Query Farcaster data with SQL.'),
-#           advanced: str = typer.Option(None, help='Query Farcaster data with Langchain\'s SQL agent.')):
-
-#     if raw:
-#         execute_raw_sql(raw)
-#     elif query:
-#         execute_natural_language_query(query)
-#     elif advanced:
-#         execute_advanced_query(advanced)
-#     else:
-#         typer.echo("Please provide either --raw, --query, or --advanced option.")
-
-
 @app.command()
 def query(query: str = typer.Argument(None, help='Query Farcaster data with natural language.'),
           raw: str = typer.Option(None, help='Query Farcaster data with SQL.'),
           advanced: str = typer.Option(None, help='For testing purposes.'),
           csv: bool = typer.Option(False, help='Save the result to a CSV file. Format: {unix_timestamp}.csv')):
+
+    if not openai_api_key:
+        print("Error: you need to set the environment variable OPENAI_API_KEY. Run `python main.py init` to do so.")
+        return
 
     if raw:
         df = execute_raw_sql(raw)

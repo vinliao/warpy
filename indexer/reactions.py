cb0956504e04d9f747a78f8825bbae9cb1664ef9
Vincent Liao <vincent.liao@protonmail.com>
2023-04-01 14:43:52 +0700

    update: handle cursor when fetching reactions

--- indexer/reactions.py
+++ indexer/reactions.py
@@ -19,19 +19,34 @@ warpcast_hub_key = os.getenv("WARPCAST_HUB_KEY")
 # ====================== WARPCAST ============================
 # ============================================================
 
-
-async def fetch_reactions(session, url, headers):
+async def fetch_reactions(session, base_url, headers):
+    reactions = []
+    cursor = None
     while True:
         try:
+            # Add the cursor query parameter to the URL if it exists
+            url = f"{base_url}&cursor={cursor}" if cursor else base_url
+
             timeout = aiohttp.ClientTimeout(total=10)
             async with session.get(url, headers=headers, timeout=timeout) as response:
                 # Sleep between requests to avoid rate limiting
                 await asyncio.sleep(1)
                 response.raise_for_status()
-                return await response.json()
+                data = await response.json()
+
+                # Append reactions to the list
+                reactions.extend(data['result']['reactions'])
+
+                # Check if there's a next page
+                cursor = data.get('next', {}).get('cursor')
+                if cursor is None:
+                    break
+
         except (aiohttp.ClientResponseError, asyncio.TimeoutError) as e:
             print(f"Error occurred for {url}: {e}. Retrying...")
             await asyncio.sleep(5)  # Wait for 5 seconds before retrying
+
+    return reactions
 
 
 async def get_cast_reactions_async(cast_hashes, warpcast_hub_key, n):
@@ -45,12 +60,10 @@ async def get_cast_reactions_async(cast_hashes, warpcast_hub_key, n):
                 tasks.append(fetch_reactions(session, url, headers))
 
             responses = await asyncio.gather(*tasks)
-            for cast_hash, response in zip(cast_hashes[i:i + n], responses):
-                data = response['result']['reactions']
-                if data is not None and data != []:
-                    print(data)
+            for cast_hash, response_data in zip(cast_hashes[i:i + n], responses):
+                if response_data is not None and response_data != []:
                     reactions[cast_hash] = [extract_reactions(
-                        item) for item in data if item]
+                        reaction) for reaction in response_data if reaction]
 
         return reactions
 
@@ -68,13 +81,6 @@ def extract_reactions(data):
         target_hash=data['castHash'],
         author_fid=data['reactor']['fid'],
     )
-
-
-# def insert_reactions(session, reactions):
-#     for reaction_list in reactions.values():
-#         for reaction in reaction_list:
-#             session.add(reaction)
-#     session.commit()
 
 
 def insert_reactions(session, reactions):
@@ -104,11 +110,14 @@ async def main():
     engine = create_engine('sqlite:///' + db_path)
 
     with sessionmaker(engine)() as session:
-        casts = session.query(Cast).filter(
-            Cast.timestamp > datetime.now().timestamp() * 1000 - 7 * 24 * 60 * 60 * 1000).all()
-        cast_hashes = [cast.hash for cast in casts]
+        casts = session.query(Cast).limit(3).all()
+        cast_hashes = ['0x6c5dea44f96bd0fdcccf0dc9b8d506115cb35734']
+        cast_hashes.extend([cast.hash for cast in casts])
 
         reactions = await get_cast_reactions(cast_hashes, warpcast_hub_key, n=35)
+
+        for reaction in reactions:
+            print(len(reactions[reaction]))
 
         # dump to db
         insert_reactions(session, reactions)

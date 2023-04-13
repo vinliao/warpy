8ba8f926ff73919f4350244c0c39ee97f694952a
Vincent Liao <vincent.liao@protonmail.com>
2023-04-13 12:39:57 +0700

    update: fetch all address when fetching eth transaction

--- indexer/eth.py
+++ indexer/eth.py
@@ -1,7 +1,6 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import func
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker
 
@@ -14,14 +13,20 @@ load_dotenv()
 
 async def main(engine: Engine):
     with sessionmaker(bind=engine)() as session:
-        # get three random external addresses
-        addresses = (
-            session.query(ExternalAddress).order_by(func.random()).limit(3).all()
-        )
-
+        # TODO: figure out a more robust algorithm for this
+        addresses = session.query(ExternalAddress).all()
         addresses_string = [address.address for address in addresses]
 
-        fetcher = AlchemyTransactionFetcher(key=os.getenv("ALCHEMY_API_KEY"))
-        data = await fetcher.fetch_data(addresses_string)
-        transactions = fetcher.get_models(data)
-        save_objects(session, transactions)
+        alchemy_api_key = os.getenv("ALCHEMY_API_KEY")
+        if not alchemy_api_key:
+            raise ValueError("Missing ALCHEMY_API_KEY")
+
+        fetcher = AlchemyTransactionFetcher(key=alchemy_api_key)
+
+        batch_size = 5
+        for i in range(0, len(addresses_string), batch_size):
+            batch = addresses_string[i : i + batch_size]  # noqa: E203
+
+            data = await fetcher.fetch_data(batch)
+            transactions = fetcher.get_models(data)
+            save_objects(session, transactions)

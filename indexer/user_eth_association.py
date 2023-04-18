93a49ff3bd4441f76574d4030d6244476f545b52
Vincent Liao <vincent.liao@protonmail.com>
2023-04-18 17:38:27 +0700

    update: make association table maker more efficient

--- indexer/user_eth_association.py
+++ indexer/user_eth_association.py
@@ -1,43 +1,105 @@
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.sql import or_
 
 from utils.models import EthTransaction, User, user_eth_transactions_association
+from sqlalchemy.orm import joinedload
+
+from collections import defaultdict
+from sqlalchemy import func, text
 
 
-def check_and_associate(
-    session: Session, user_fid: int, user_address: str, tx: EthTransaction
-):
-    associations_to_create = []
+def delete_duplicate_associations(session: Session):
+    # Create a CTE to identify duplicate associations
+    duplicates_cte = session.query(
+        user_eth_transactions_association.c.user_fid,
+        user_eth_transactions_association.c.eth_transaction_unique_id,
+        func.row_number()
+        .over(
+            partition_by=[
+                user_eth_transactions_association.c.user_fid,
+                user_eth_transactions_association.c.eth_transaction_unique_id,
+            ],
+            order_by=user_eth_transactions_association.c.user_fid,
+        )
+        .label("row_number"),
+    ).cte("duplicates_cte")
 
-    if tx.to_address == user_address or tx.from_address == user_address:
-        existing_association = (
-            session.query(user_eth_transactions_association)
-            .filter_by(user_fid=user_fid, eth_transaction_unique_id=tx.unique_id)
-            .first()
+    # Join the CTE with the association table and delete the duplicate rows
+    affected_rows = (
+        session.query(user_eth_transactions_association)
+        .filter(
+            text(
+                f"{user_eth_transactions_association.c.user_fid} = {duplicates_cte.c.user_fid} AND "
+                f"{user_eth_transactions_association.c.eth_transaction_unique_id} = {duplicates_cte.c.eth_transaction_unique_id} AND "
+                "duplicates_cte.row_number > 1"
+            )
+        )
+        .delete(synchronize_session=False)
+    )
+
+    print(f"Deleted {affected_rows} duplicate rows")
+    session.commit()
+
+
+def process_tx_batch(session, tx_batch, user_address_cache):
+    unique_addresses = set(tx.from_address for tx in tx_batch).union(
+        tx.to_address for tx in tx_batch
+    )
+
+    # Remove addresses already present in the cache
+    uncached_addresses = unique_addresses - set(user_address_cache.keys())
+
+    # Query uncached addresses
+    if uncached_addresses:
+        new_users = (
+            session.query(User).filter(User.address.in_(uncached_addresses)).all()
         )
 
-        if not existing_association:
+        # Add new users to the cache
+        for user in new_users:
+            user_address_cache[user.address] = user.fid
+
+    associations_to_create = []
+    for tx in tx_batch:
+        from_fid = user_address_cache.get(tx.from_address)
+        to_fid = user_address_cache.get(tx.to_address)
+
+        if from_fid:
             associations_to_create.append(
-                {
-                    "user_fid": user_fid,
-                    "eth_transaction_unique_id": tx.unique_id,
-                }
+                {"user_fid": from_fid, "eth_transaction_unique_id": tx.unique_id}
             )
-            print(
-                f"Association to be created for user {user_fid} and transaction {tx.unique_id}"
+        if to_fid and to_fid != from_fid:
+            associations_to_create.append(
+                {"user_fid": to_fid, "eth_transaction_unique_id": tx.unique_id}
             )
-        else:
-            print(
-                f"Association already exists for user {user_fid} and transaction {tx.unique_id}"
+
+    print(f"Creating {len(associations_to_create)} associations...")
+
+    if associations_to_create:
+        for association in associations_to_create:
+            session.execute(
+                user_eth_transactions_association.insert().values(**association)
             )
-    return associations_to_create
+        session.commit()
 
 
 def main(engine: Engine):
-    with sessionmaker(bind=engine)() as session:
-        users = session.query(User).all()
-        txs = session.query(EthTransaction).all()
+    batch_size = 5000  # Decrease batch size to reduce unique_addresses count
+    user_address_cache = defaultdict(int)
 
-        for user in users:
-            for tx in txs:
-                check_and_associate(session, user.fid, user.address, tx)
+    with sessionmaker(bind=engine)() as session:
+        tx_count = session.query(EthTransaction).count()
+
+        for tx_offset in range(370000, tx_count, batch_size):
+            tx_batch = (
+                session.query(EthTransaction)
+                .order_by(EthTransaction.unique_id)
+                .offset(tx_offset)
+                .limit(batch_size)
+                .all()
+            )
+
+            print(f"Current offset: {tx_offset}")
+
+            process_tx_batch(session, tx_batch, user_address_cache)
